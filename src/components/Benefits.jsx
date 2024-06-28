8b31717fcaccb9e21fa15419adb19fc5772bce74
AbdElRahman KahlOut <akahlouts@gmail.com>
2024-06-28 17:56:06 +0300

    fix some bugs in svg

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -20,11 +20,7 @@ const Benefits = () => {
           {benefits.map((item) => (
             <div
               className="block relative p-0.5 bg-no-repeat bg-[length:100%_100%] md:max-w-[24rem]"
-              style={{
-                backgroundImage: `url('/src/assets/benefits/card-${
-                  parseInt(item.id) + 1
-                }.svg')`,
-              }}
+              style={{ backgroundImage: `url(${item.backgroundUrl})` }}
               key={item.id}
             >
               <div className="relative z-2 flex flex-col min-h-[22rem] p-[2.4rem] pointer-events-none">
